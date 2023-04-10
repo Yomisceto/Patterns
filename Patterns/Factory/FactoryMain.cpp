0194065c5150a1bd58c7ae482baf473050d94c47
Yomisceto <yomisceto@gmail.com>
2023-04-10 14:18:59 +0300

    added Singleton

--- Patterns/Factory/FactoryMain.cpp
+++ Patterns/Factory/FactoryMain.cpp
@@ -1,12 +1,12 @@
 #include "PizzaStore.h" 
 
-
-int main() {
-
-	PizzaStore* pPizza = new NYPizzaStore;
-	pPizza->orderPizza(PizzaStore::PizzaType::CheesePiza);
-	std::cout << std::endl;
-
-	delete pPizza;
-	return 0;
-}
+//
+//int main() {
+//
+//	PizzaStore* pPizza = new NYPizzaStore;
+//	pPizza->orderPizza(PizzaStore::PizzaType::CheesePiza);
+//	std::cout << std::endl;
+//
+//	delete pPizza;
+//	return 0;
+//}
---
+++ Patterns/Singleton/SingletonMain.cpp
@@ -0,0 +1,23 @@
+
+#include "Singleton.h"
+#include <iostream>
+#include <thread>
+
+void ThreadFoo() {
+	const Singleton* singleton = Singleton::GetInstance("Foo");
+	std::cout << singleton->value() << std::endl;
+
+}
+
+void ThreadBar() {
+	const Singleton* singleton = Singleton::GetInstance("Bar");
+	std::cout << singleton->value() << std::endl;
+}
+
+int main() {
+	std::thread t1(ThreadFoo);
+	std::thread t2(ThreadBar);
+	t1.join();
+	t2.join();
+	
+}
